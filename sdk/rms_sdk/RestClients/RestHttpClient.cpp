9a0501c0b5a7b73f92e384539861f62c42772f62
AgentRX <andrey.pogoreltsev@gmail.com>
2015-09-10 16:37:09 +0300

    DevicePlatform changed to WindowsStore

--- sdk/rms_sdk/RestClients/RestHttpClient.cpp
+++ sdk/rms_sdk/RestClients/RestHttpClient.cpp
@@ -164,7 +164,7 @@ string RestHttpClient::GenerateRequestId()
 string RestHttpClient::GetPlatformIdHeader()
 {
   string platformId(
-    "AppName=rmscore;AppVersion=1.0;DevicePlatform=Web;SDKVersion=4.1;");
+    "AppName=rmscore;AppVersion=1.0;DevicePlatform=WindowsStore;SDKVersion=4.1;");
 
   return platformId;
 }
