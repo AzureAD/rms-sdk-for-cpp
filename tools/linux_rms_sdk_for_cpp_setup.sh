e63243b46cf1bd329d0a6ebb1ba7af35fd2577e5
Adolfo <muller.adolfo@gmail.com>
2016-12-16 15:50:06 -0800

    changes to setup script

--- tools/linux_rms_sdk_for_cpp_setup.sh
+++ tools/linux_rms_sdk_for_cpp_setup.sh
@@ -10,5 +10,5 @@ cd tests
 ./rmscryptoUnitTests -xunitxml > rmsUnitTestResults.txt
 ./rmsauthUnitTests -xunitxml >> rmsUnitTestResults.txt
 ./rmsplatformUnitTests -xunitxml >> rmsUnitTestResults.txt
-cd ../tools
+cd ../../tools
 powershell ./Rms_Sdk_For_Cpp_UTResults_Parser.ps1
