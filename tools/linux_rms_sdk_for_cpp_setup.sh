1ddc1f8d0164f781d2930488e3ace1d7562593c4
Adolfo <muller.adolfo@gmail.com>
2017-01-04 11:10:30 -0800

    style changes

--- tools/linux_rms_sdk_for_cpp_setup.sh
+++ tools/linux_rms_sdk_for_cpp_setup.sh
@@ -10,6 +10,6 @@ cd tests
 ./rmscryptoUnitTests -xunitxml > rmsUnitTestResults.txt
 ./rmsauthUnitTests -xunitxml >> rmsUnitTestResults.txt
 ./rmsplatformUnitTests -xunitxml >> rmsUnitTestResults.txt
-./RestClientsUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
+./RestClientsUnitTests -xunitxml >> rmsUnitTestResults.txt
 cd ../../tools
 powershell ./Rms_Sdk_For_Cpp_UTResults_Parser.ps1
