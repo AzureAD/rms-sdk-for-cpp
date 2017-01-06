51130de22d83b0ad6b99029bf6c30afb033179fb
Anurag Pratik <anuragprateek@gmail.com>
2017-01-06 14:16:29 -0800

    making win build script configurable

--- tools/windows_build.bat
+++ tools/windows_build.bat
@@ -1,17 +1,19 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+set third_party_path=%1
+set qmake_path=%2
 mkdir ..\third_party
-xcopy C:\third_party ..\third_party /e
+xcopy %third_party_path% ..\third_party /e
 cd ..\sdk
-call C:\Qt\5.7\msvc2015\bin\qmake.exe sdk.pro -spec win32-msvc2015 "CONFIG+=debug_and_release" 
+call %qmake_path%\qmake.exe sdk.pro -spec win32-msvc2015 "CONFIG+=debug_and_release" 
 call C:\Qt\Tools\QtCreator\bin\jom.exe clean
 call C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..\samples
-call C:\Qt\5.7\msvc2015\bin\qmake.exe samples.pro -spec win32-msvc2015 "CONFIG+=debug_and_release" 
+call %qmake_path%\qmake.exe samples.pro -spec win32-msvc2015 "CONFIG+=debug_and_release" 
 call C:\Qt\Tools\QtCreator\bin\jom.exe clean
 call C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..\bin
 copy tests\*UnitTest* .
-xcopy C:\Qt\5.7\msvc2015\bin\*.dll .
+xcopy %qmake_path%\*.dll .
 rmscryptoUnitTests.exe -xunitxml > tests\rmsUnitTestResults.txt
 ::rmsauthUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
 rmsplatformUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
