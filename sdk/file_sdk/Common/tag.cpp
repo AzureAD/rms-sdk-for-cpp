47de99a06252e1bbced8d7c2e8407ad233b68ec9
Shenhav Baruch El <shenhavba@gmail.com>
2017-08-31 13:44:23 +0300

    Task #1450266: Implement GetTags function

--- sdk/file_sdk/Common/tag.cpp
+++ sdk/file_sdk/Common/tag.cpp
@@ -1,5 +1,6 @@
 #include "tag.h"
 #include <regex>
+#include <string_utils.h>
 
 using std::pair;
 using std::string;
@@ -11,26 +12,6 @@ static const string kExtendedPrefix = "Extended";
 }  // namespace
 
 namespace mip {
-
-static bool TryParseBool(const string& str, bool& result) {
-  // Perf nit: We do not need a new string that is lower case. We can do
-  // a case insensitive comparison with "true" or "false". That has two (nit)
-  // perf benefits: another string is not allocated, time complexity is always
-  // O(1) since parsing will only do maximum 5 comparisions ("false").
-  // TODO: Find a portable way to do case insensitive comparison.
-  string s(str);
-  transform(s.begin(), s.end(), s.begin(), ::tolower);
-  // Currently we do not ignore leading and trailing whitespace.
-  // Add that if needed, potentially controlled by a flag passed to the function.
-  if  (s == "true") {
-    result = true;
-    return true;
-  } else if (s == "false") {
-    result = false;
-    return true;
-  }
-  return false;
-}
 
 // static
 const string& Tag::GetMetaDataPrefix() {
--- sdk/file_sdk/UnitTests/xmp_ut/xmp_file_tests.cpp
+++ sdk/file_sdk/UnitTests/xmp_ut/xmp_file_tests.cpp
@@ -3,8 +3,15 @@
 #include <QtTest>
 #include <QTStreamImpl.h>
 #include <XMPFileFormat/xmp_file_format.h>
+#include <CryptoAPI.h>
+#include <sstream>
+#include <fstream>
 
+using rmscrypto::api::IStream;
+using std::static_pointer_cast;
+using std::make_shared;
 using mip::file::XMPFileFormat;
+using mip::Tag;
 
 class Xmp_Tests : public QObject
 {
@@ -14,28 +21,90 @@ public:
   Xmp_Tests();
 
 private Q_SLOTS:
-  void GetTags_FileWithTag_ReturnCorrectTag();
+  void GetTags_FileWithAutomaticTag_ReturnCorrectTag();
+  void GetTags_FileWithManualTag_ReturnCorrectTag();
 };
 
-Xmp_Tests::Xmp_Tests()
-{
+Xmp_Tests::Xmp_Tests() {
 }
 
-void Xmp_Tests::GetTags_FileWithTag_ReturnCorrectTag()
-{
-  /*mip::Tag general( "f42aa342-8706-4288-bd11-ebb85995028c", "General", "", "vakuras@microsoft.com", true, "", "", mip::Method::MANUAL,"72f988bf-86f1-41af-91ab-2d7cd011db47" );
+bool VerifyTags(Tag tag1, Tag tag2) {
+  if (tag1.GetExtendedProperties().size() != tag2.GetExtendedProperties().size())
+    return false;
 
-  foreach( const QString &fileName, QDir(":files/xmp_labeled").entryList() )
+  for (size_t i = 0; i < tag1.GetExtendedProperties().size(); i++) {
+    if (tag1.GetExtendedProperties()[i].key != tag2.GetExtendedProperties()[i].key ||
+        tag1.GetExtendedProperties()[i].value != tag2.GetExtendedProperties()[i].value ||
+        tag1.GetExtendedProperties()[i].vendor != tag2.GetExtendedProperties()[i].vendor) {
+      return false;
+    }
+  }
+
+  return
+      tag1.GetLabelId() == tag2.GetLabelId() &&
+      tag1.GetLabelName() == tag2.GetLabelName() &&
+      tag1.GetLabelParentId() == tag2.GetLabelParentId() &&
+      tag1.GetEnabled() == tag2.GetEnabled() &&
+      tag1.GetOwner() == tag2.GetOwner() &&
+      tag1.GetApplicationName() == tag2.GetApplicationName() &&
+      tag1.GetSiteId() == tag2.GetSiteId();
+}
+
+void Xmp_Tests::GetTags_FileWithManualTag_ReturnCorrectTag() {
+  mip::Tag general( "f42aa342-8706-4288-bd11-ebb85995028c", "", "", "vakuras@microsoft.com", true, "", "", mip::Method::MANUAL, "https://rmsibizaapidf.trafficmanager.net/api/72f988bf-86f1-41af-91ab-2d7cd011db47");
+
+  QTemporaryDir tempDir;
+  tempDir.autoRemove();
+  foreach( const QString &fileName, QDir(":xmp_labeled_manual").entryList() )
   {
-    QFile file(":files/xmp_labeled/" + fileName);
-    QSharedPointer<QDataStream> obj = QSharedPointer<QDataStream>(new QDataStream(&file));
-    auto stream = QTStreamImpl::Create(obj);
-    XMPFileFormat xmpFileFormat(stream, ".gif");
-    auto tags = xmpFileFormat.GetTags();
-    QVERIFY2(tags.size() == 1, "Tags count shoud be 1");
-    mip::Tag tag = tags[0];
-    QVERIFY2(tag == general, "Tag is different than expected");
-  }*/
+    auto fileNameString = fileName.toStdString();
+    auto extension = fileNameString.substr(fileNameString.find_last_of("."));
+    auto tempFile = tempDir.path() + "/temp" + QString::fromStdString(extension);
+    if (QFile::copy(":xmp_labeled_manual/"+fileName, tempFile)) {
+
+      auto ifs = std::make_shared<std::ifstream>(tempFile.toStdString(), std::ios_base::binary);
+      auto stream = rmscrypto::api::CreateStreamFromStdStream(std::static_pointer_cast<std::istream>(ifs));
+
+      XMPFileFormat xmpFileFormat(stream, extension);
+      auto tags = xmpFileFormat.GetTags();
+      QVERIFY2(tags.size() == 1, "Tags count shoud be 1");
+      mip::Tag tag = tags[0];
+      QVERIFY2(VerifyTags(tag, general), "Tag is different than expected");
+    }
+  }
+}
+
+void Xmp_Tests::GetTags_FileWithAutomaticTag_ReturnCorrectTag() {
+  vector<mip::ExtendedProperty> properties;
+  mip::ExtendedProperty property;
+  property.key = "Method";
+  property.vendor = "MSFT";
+  property.value = "Automatic";
+  properties.push_back(property);
+
+  mip::Tag general( "f42aa342-8706-4288-bd11-ebb85995028c", "General", "", "shbaruch@microsoft.com", true, "", "Microsoft Azure Information Protection", mip::Method::NONE, "72f988bf-86f1-41af-91ab-2d7cd011db47", properties);
+
+  QTemporaryDir tempDir;
+  tempDir.autoRemove();
+
+  foreach( const QString &fileName, QDir(":xmp_labeled_automatic").entryList() )
+  {
+    auto fileNameString = fileName.toStdString();
+    auto extension = fileNameString.substr(fileNameString.find_last_of("."));
+    auto tempFile = tempDir.path() + "/temp" +  QString::fromStdString(extension);
+    if (QFile::copy(":xmp_labeled_automatic/" + fileName, tempFile)) {
+
+      auto ifs = std::make_shared<std::ifstream>(tempFile.toStdString(), std::ios::binary);
+      auto stream = rmscrypto::api::CreateStreamFromStdStream(
+            std::static_pointer_cast<std::istream>(ifs));
+
+      XMPFileFormat xmpFileFormat(stream, extension);
+      auto tags = xmpFileFormat.GetTags();
+      QVERIFY2(tags.size() == 1, "Tags count shoud be 1");
+      mip::Tag tag = tags[0];
+      QVERIFY2(VerifyTags(tag, general), "Tag is different than expected");
+    }
+  }
 }
 
 QTEST_APPLESS_MAIN(Xmp_Tests)
