827a4de7a669dcd55f0526d46a52a1154ac67bf3
Admuller <admuller@microsoft.com>
2017-09-05 11:55:56 -0700

    able to make rmscrypto.dll

--- sdk/rmscrypto_sdk/CryptoAPI/CryptoAPIExport.h
+++ sdk/rmscrypto_sdk/CryptoAPI/CryptoAPIExport.h
@@ -12,7 +12,7 @@
 // This code produces too many warnings
 
 // #if defined _WIN32 || defined __CYGWIN__
-//   #ifdef RMS_CRYPTO_LIBRARY
+  // #ifdef RMS_CRYPTO_LIBRARY
 //     #ifdef __GNUC__
 //       #define DLL_PUBLIC_CRYPTO __attribute__ ((dllexport))
 //     #else
@@ -27,13 +27,23 @@
 //   #endif
 //   #define DLL_LOCAL
 // #else
-  // #if __GNUC__ >= 4
-  //   #define DLL_PUBLIC_CRYPTO __attribute__ ((visibility ("default")))
-  //   #define DLL_LOCAL_CRYPTO  __attribute__ ((visibility ("hidden")))
-  // #else
-    #define DLL_PUBLIC_CRYPTO
-    #define DLL_LOCAL_CRYPTO
+//   #if __GNUC__ >= 4
+//     #define DLL_PUBLIC_CRYPTO __attribute__ ((visibility ("default")))
+//     #define DLL_LOCAL_CRYPTO  __attribute__ ((visibility ("hidden")))
+//   #else
+//     #define DLL_PUBLIC_CRYPTO
+//     #define DLL_LOCAL_CRYPTO
 //   #endif
 // #endif
 
+
+#ifdef _WIN32
+
+  #ifdef RMS_CRYPTO_LIBRARY
+    #define DLL_PUBLIC_CRYPTO __declspec(dllexport)
+  #else
+    #define DLL_PUBLIC_CRYPTO __declspec(dllimport)
+  #endif  // ifdef UPE_IMPLEMENTATION
+#endif
+ 
 #endif  // _RMS_LIB_EXPORT_H_
