2828fad4b334d26d003e4d34204e6eee059fad95
Anurag Pratik <anuprat@microsoft.com>
2017-09-11 11:49:08 -0700

    Updating windows_build.bat for VC13

--- tools/windows_build.bat
+++ tools/windows_build.bat
@@ -1,18 +1,18 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 set third_party_path=%1
 set qmake_path=%2
 set platform=%3
 if %platform%==amd64 (
-	call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+	call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 )
 mkdir ..\third_party
 xcopy %third_party_path% ..\third_party /e
 cd ..\sdk
-call %qmake_path%\qmake.exe sdk.pro -spec win32-msvc2015 "CONFIG+=release" 
+call %qmake_path%\qmake.exe sdk.pro -spec win32-msvc2013 "CONFIG+=release" 
 call C:\Qt\Tools\QtCreator\bin\jom.exe clean
 call C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..\samples
-call %qmake_path%\qmake.exe samples.pro -spec win32-msvc2015 "CONFIG+=release" 
+call %qmake_path%\qmake.exe samples.pro -spec win32-msvc2013 "CONFIG+=release" 
 call C:\Qt\Tools\QtCreator\bin\jom.exe clean
 call C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..\bin
