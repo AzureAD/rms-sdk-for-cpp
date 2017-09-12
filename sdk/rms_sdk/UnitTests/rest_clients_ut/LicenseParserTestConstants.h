05d6207ed0c65f9ad3d08f2bbceb3897a9cea0ca
Ran Eitan <raeitan@microsoft.com>
2017-09-11 18:51:24 -0700

    uint8_t to char

--- sdk/rms_sdk/UnitTests/rest_clients_ut/LicenseParserTestConstants.h
+++ sdk/rms_sdk/UnitTests/rest_clients_ut/LicenseParserTestConstants.h
@@ -3,10 +3,10 @@
 
 #include <QtTest>
 
-extern const uint8_t PL_0101right_ECB_xml[];
+extern const char PL_0101right_ECB_xml[];
 extern const unsigned int PL_0101right_ECB_xml_len;
 
-extern const uint8_t PL_0101right_CBC_xml[];
+extern const char PL_0101right_CBC_xml[];
 extern const unsigned int PL_0101right_CBC_xml_len;
 #endif // LICENSEPARSERTESTCONSTANTS_H_
 
