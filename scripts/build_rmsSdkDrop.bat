7886c007d5dd839e440d00b2ff83d0fb9e1c466b
Anurag Pratik <anuprat@microsoft.com>
2017-09-12 17:49:42 -0700

    Changing scripts to enable debug builds

--- scripts/build_rmsSdkDrop.bat
+++ scripts/build_rmsSdkDrop.bat
@@ -2,6 +2,7 @@
 
 set buildtarget=%1
 set msvcver=%2
+set build_type=%3
 
 IF /I "%buildtarget%"=="amd64" (
 	IF /I "%msvcver%"=="msvc2015" (
@@ -24,28 +25,35 @@ IF /I "%buildtarget%"=="x86" (
 if exist rmsSdkDeploy\ rd /q /s rmsSdkDeploy\
 call mkdir rmsSdkDeploy
 
-call copy bin\rms.dll rmsSdkDeploy\.
-call copy bin\rms.lib rmsSdkDeploy\.
-call copy bin\rms.exp rmsSdkDeploy\.
-call copy bin\rmscrypto.dll rmsSdkDeploy\.
-call copy bin\rmscrypto.lib rmsSdkDeploy\.
-call copy bin\rmscrypto.exp rmsSdkDeploy\.
-call copy bin\rmsd.dll rmsSdkDeploy\.
-call copy bin\rmsd.lib rmsSdkDeploy\.
-call copy bin\rmsd.exp rmsSdkDeploy\.
-call copy bin\rmsd.pdb rmsSdkDeploy\.
-call copy bin\rmscryptod.dll rmsSdkDeploy\.
-call copy bin\rmscryptod.lib rmsSdkDeploy\.
-call copy bin\rmscryptod.exp rmsSdkDeploy\.
-call copy bin\rmscryptod.pdb rmsSdkDeploy\.
+IF /I "%build_type"=="debug" (
+	call copy bin\rmsd.dll rmsSdkDeploy\.
+	call copy bin\rmsd.lib rmsSdkDeploy\.
+	call copy bin\rmsd.exp rmsSdkDeploy\.
+	call copy bin\rmsd.pdb rmsSdkDeploy\.
+	call copy bin\rmscryptod.dll rmsSdkDeploy\.
+	call copy bin\rmscryptod.lib rmsSdkDeploy\.
+	call copy bin\rmscryptod.exp rmsSdkDeploy\.
+	call copy bin\rmscryptod.pdb rmsSdkDeploy\.
+
+	call cd rmsSdkDeploy
+	call C:\Qt\5.7\%windeployqtpath%\bin\windeployqt.exe rmsd.dll rmscryptod.dll
+)
+
+IF /I "%build_type"=="release" (
+	call copy bin\rms.dll rmsSdkDeploy\.
+	call copy bin\rms.lib rmsSdkDeploy\.
+	call copy bin\rms.exp rmsSdkDeploy\.
+	call copy bin\rmscrypto.dll rmsSdkDeploy\.
+	call copy bin\rmscrypto.lib rmsSdkDeploy\.
+	call copy bin\rmscrypto.exp rmsSdkDeploy\.
+	
+	call cd rmsSdkDeploy
+	call C:\Qt\5.7\%windeployqtpath%\bin\windeployqt.exe rms.dll rmscrypto.dll
+)
+
+call cd ..
 
 call copy third_party\lib\eay\ssleay32.dll rmsSdkDeploy\.
 call copy third_party\lib\eay\libeay32.dll rmsSdkDeploy\.
 
 call copy "THIRD PARTY NOTICES - Azure Info Protection SDK (w QT Libraries).txt" rmsSdkDeploy\.
-
-call cd rmsSdkDeploy
-call C:\Qt\5.7\%windeployqtpath%\bin\windeployqt.exe rms.dll rmscrypto.dll
-call C:\Qt\5.7\%windeployqtpath%\bin\windeployqt.exe rmsd.dll rmscryptod.dll
-
-call cd ..
--- tools/windows_build.bat
+++ tools/windows_build.bat
@@ -2,25 +2,36 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd
 set third_party_path=%1
 set qmake_path=%2
 set platform=%3
+set build_type=%4
 if %platform%==amd64 (
 	call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
 )
 mkdir ..\third_party
 xcopy %third_party_path% ..\third_party /e
 cd ..\sdk
-call %qmake_path%\qmake.exe sdk.pro -spec win32-msvc2013 "CONFIG+=release" 
+call %qmake_path%\qmake.exe sdk.pro -spec win32-msvc2013 "CONFIG+="%build_type% 
 call C:\Qt\Tools\QtCreator\bin\jom.exe clean
 call C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..\samples
-call %qmake_path%\qmake.exe samples.pro -spec win32-msvc2013 "CONFIG+=release" 
+call %qmake_path%\qmake.exe samples.pro -spec win32-msvc2013 "CONFIG+="%build_type% 
 call C:\Qt\Tools\QtCreator\bin\jom.exe clean
 call C:\Qt\Tools\QtCreator\bin\jom.exe
 cd ..\bin
 copy tests\*UnitTest* .
 set PATH=%qmake_path%;%PATH%
 set PATH=..\third_party\lib\eay;%PATH%
-rmscryptoUnitTests.exe -xunitxml > tests\rmsUnitTestResults.txt
-::rmsauthUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
-rmsplatformUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
-RestClientsUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
+
+IF /I "%build_type"=="debug" (
+  rmscryptoUnitTestsd.exe -xunitxml > tests\rmsUnitTestResults.txt
+  ::rmsauthUnitTestsd.exe -xunitxml >> tests\rmsUnitTestResults.txt
+  rmsplatformUnitTestsd.exe -xunitxml >> tests\rmsUnitTestResults.txt
+  RestClientsUnitTestsd.exe -xunitxml >> tests\rmsUnitTestResults.txt
+)
+IF /I "%build_type"=="release" (
+  rmscryptoUnitTests.exe -xunitxml > tests\rmsUnitTestResults.txt
+  ::rmsauthUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
+  rmsplatformUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
+  RestClientsUnitTests.exe -xunitxml >> tests\rmsUnitTestResults.txt
+)
+
 cd ..
