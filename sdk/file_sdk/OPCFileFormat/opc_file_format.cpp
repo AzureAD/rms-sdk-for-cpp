e4204d8b7ffca5418f8df9452d0719bfdb87e28c
Shenhav Baruch El <shenhavba@gmail.com>
2017-09-13 15:02:22 +0300

    Task #1450265: Implement SetTags function

--- sdk/file_sdk/OPCFileFormat/opc_file_format.cpp
+++ sdk/file_sdk/OPCFileFormat/opc_file_format.cpp
@@ -3,8 +3,8 @@
 namespace mip {
 namespace file {
 
-OPCFileFormat::OPCFileFormat(std::shared_ptr<IStream> file, const string& extension)
-  : FileFormat(file, extension)
+OPCFileFormat::OPCFileFormat(std::shared_ptr<IStream> inputStream, const string& extension)
+  : FileFormat(inputStream, extension)
 {
 }
 
@@ -12,7 +12,7 @@ const vector<Tag> OPCFileFormat::ReadTags() {
   throw std::runtime_error("not implemented");
 }
 
-void OPCFileFormat::Commit(std::shared_ptr<IStream> file, string& newExtension) {
+void OPCFileFormat::Commit(std::shared_ptr<IStream> outputStream, string& newExtension) {
   throw std::runtime_error("not implemented");
 }
 
