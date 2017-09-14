4f60f2fabc948c020012a1c1bb8c152cd15307bb
Adolfo Muller <admuller@microsoft.com>
2017-09-14 13:56:50 -0700

    fixing build

--- tools/linux_rms_sdk_for_cpp_setup.sh
+++ tools/linux_rms_sdk_for_cpp_setup.sh
@@ -7,7 +7,6 @@ if [ $? -ne 0 ]; then
   echo "sdk make failed."
   exit 1
 fi
-echo "SDK done."
 cd ../samples
 qmake -recursive
 make clean
@@ -16,16 +15,44 @@ if [ $? -ne 0 ]; then
   echo "sdk make failed."
   exit 1
 fi
-echo "Samples done."
-export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 cd ../bin
+export XKB_DEFAULT_RULES=base
+export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 export LD_LIBRARY_PATH=`pwd`
 cd tests
 ./rmscryptoUnitTests -xunitxml > rmsUnitTestResults.txt
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
 ./rmsauthUnitTests -xunitxml >> rmsUnitTestResults.txt
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
 ./rmsplatformUnitTests -xunitxml >> rmsUnitTestResults.txt
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
 ./RestClientsUnitTests -xunitxml >> rmsUnitTestResults.txt
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
 ./xmpFileUnitTests -xunitxml >> rmsUnitTestResults.txt
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
 ./commonUnitTests -xunitxml >> rmsUnitTestResults.txt
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
 cd ../../tools
 powershell ./Rms_Sdk_For_Cpp_UTResults_Parser.ps1
+if [ $? -ne 0 ]; then
+  echo "sdk make failed."
+  exit 1
+fi
