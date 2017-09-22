7b603df256762aa51e57c5c4368cd1f60f57e495
Admuller <admuller@microsoft.com>
2017-09-22 11:37:34 -0700

    Changes scons_build.bat

--- scripts/scons_build.bat
+++ scripts/scons_build.bat
@@ -1,5 +1,5 @@
 @echo off
-
+SETLOCAL enabledelayedexpansion 
 REM set third_party_path=%1
 REM set qmake_path=%2
 REM set platform=%3
@@ -7,58 +7,49 @@ set DIRECTORY_NAME=%1
 
 rmdir /Q /S %DIRECTORY_NAME%
 mkdir %DIRECTORY_NAME%
+mkdir %DIRECTORY_NAME%\release
+mkdir %DIRECTORY_NAME%\release\amd64
+mkdir %DIRECTORY_NAME%\release\x86
+mkdir %DIRECTORY_NAME%\debug
+mkdir %DIRECTORY_NAME%\debug\amd64
+mkdir %DIRECTORY_NAME%\debug\x86
 
-rmdir /Q /S bin
-rmdir /Q /S third_party
-mkdir third_party
-xcopy "C:\third_party_msvc15win64" third_party /e /y
-
-set PATH=C:\Qt\5.7\msvc2015\bin;C:\Qt\5.7\msvc2015_64\bin;%PATH%
-set PATH=third_party\lib\eay;third_party\lib\Libgsf;third_party\lib\xmp;%PATH%
+set archs=x64 x86
+set configs=release debug
 
 call rmdir /Q /S unittest_results
 call mkdir unittest_results
 
-call scons --arch=x64 --configuration=release --package -c
-call scons --arch=x64 --configuration=release --package
-if %ERRORLEVEL% NEQ 0 ( 
-	exit /B 1
-)
-call scripts\\scons_unittest_helper.bat bin\\release\\amd64\\sdk unittest_results release_amd64_results.txt
-call mkdir %DIRECTORY_NAME%\\release
-call mkdir %DIRECTORY_NAME%\\release\\amd64
-call copy bin\\release\\amd64\\sdk\\rms*.dll %DIRECTORY_NAME%\\release\\amd64
+set PATH=C:\Qt\5.7\msvc2015\bin;C:\Qt\5.7\msvc2015_64\bin;%PATH%
+set PATH=third_party\lib\eay;third_party\lib\Libgsf;third_party\lib\xmp;%PATH%
 
-call scons --arch=x64 --configuration=debug --package -c
-call scons --arch=x64 --configuration=debug --package
-if %ERRORLEVEL% NEQ 0 ( 
-	exit /B 1
+for %%a in (%archs%) do (
+	rmdir /Q /S third_party\include
+	rmdir /Q /S third_party\lib
+	if %%a == x64 (
+		xcopy "C:\third_party_msvc15win64" third_party /e /y
+	)
+	if %%a == x86 (
+		xcopy "C:\third_party_msvc15win32" third_party /e /y
+	)
+	for %%c in  (%configs%) do (
+		echo scons --arch=%%a --configuration=%%c --package
+		call scons --arch=%%a --configuration=%%c --package -c
+		call scons --arch=%%a --configuration=%%c --package
+		if %ERRORLEVEL% NEQ 0 (
+			exit /B 1
+		)
+		if %%a == x64 (
+			set arch_path=amd64
+		) else (
+			set arch_path=x86
+		)
+		echo scripts\scons_unittest_helper.bat bin\%%c\!arch_path!\sdk unittest_results %%c_!arch_path!_results
+		echo copy bin\%%c\!arch_path!\sdk\rms*.dll %DIRECTORY_NAME%\%%c\!arch_path!
+		call scripts\scons_unittest_helper.bat bin\%%c\!arch_path!\sdk unittest_results %%c_!arch_path!_results
+		if %ERRORLEVEL% NEQ 0 (
+			exit /B 1
+		)
+		call copy bin\%%c\!arch_path!\sdk\rms*.dll %DIRECTORY_NAME%\%%c\!arch_path!
+	)
 )
-call scripts\\scons_unittest_helper.bat bin\\debug\\amd64\\sdk unittest_results debug_amd64_results.txt
-call mkdir %DIRECTORY_NAME%\\debug
-call mkdir %DIRECTORY_NAME%\\debug\\amd64
-call copy bin\\debug\\amd64\\sdk\\rms*.dll %DIRECTORY_NAME%\\debug\\amd64
-
-rmdir /Q /S third_party
-mkdir third_party
-xcopy "C:\third_party_msvc15win32" third_party /e /y
-
-call scons --arch=x86 --configuration=release --package -c
-call scons --arch=x86 --configuration=release --package
-if %ERRORLEVEL% NEQ 0 ( 
-	exit /B 1
-)
-call scripts\\scons_unittest_helper.bat bin\\release\\x86\\sdk unittest_results release_x86_results.txt
-call mkdir %DIRECTORY_NAME%\\release
-call mkdir %DIRECTORY_NAME%\\release\\x86
-call copy bin\\release\\x86\\sdk\\rms*.dll %DIRECTORY_NAME%\\release\\x86
-
-call scons --arch=x86 --configuration=debug --package -c
-call scons --arch=x86 --configuration=debug --package
-if %ERRORLEVEL% NEQ 0 ( 
-	exit /B 1
-)
-call scripts\\scons_unittest_helper.bat bin\\debug\\x86\\sdk unittest_results debug_x86_results.txt
-call mkdir %DIRECTORY_NAME%\\debug
-call mkdir %DIRECTORY_NAME%\\debug\\x86
-call copy bin\\debug\\x86\\sdk\\rms*.dll %DIRECTORY_NAME%\\debug\\x86
