b909d7c87a6f6c502f43b4097ff299d439467d9f
David Schuldenfrei <david.schuldenfrei@gmail.com>
2017-09-28 17:30:49 +0300

    Abrahams comments

--- sdk/file_sdk/OPCFileFormat/test/custom_properties_tests.cpp
+++ sdk/file_sdk/OPCFileFormat/test/custom_properties_tests.cpp
@@ -11,8 +11,8 @@ const string emptyXml = R"(<?xml version="1.0" encoding="UTF-8" standalone="yes"
 
 TEST(CustomProperties, GetProperties_EmptyProperties_EmptyTags) {
   CustomPropertiesXml customProperties { emptyXml };
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(0, tags.size());
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(0, properties.size());
 }
 
 const string singlePropertyXml = R"(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -24,9 +24,9 @@ const string singlePropertyXml = R"(<?xml version="1.0" encoding="UTF-8" standal
 
 TEST(CustomProperties, GetProperties_SingleProperty_SingleTag) {
   CustomPropertiesXml customProperties{ singlePropertyXml };
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(1, tags.size());
-  EXPECT_EQ(tags[0], CustomProperty("PropName", "PropValue"));
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(1, properties.size());
+  EXPECT_EQ(properties[0], CustomProperty("PropName", "PropValue"));
 }
 
 const string multiPropertyXml = R"(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -43,11 +43,11 @@ const string multiPropertyXml = R"(<?xml version="1.0" encoding="UTF-8" standalo
 </Properties>)";
 TEST(CustomProperties, GetProperties_MultipleProperties_MultipleTags) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(3, tags.size());
-  EXPECT_EQ(tags[0], CustomProperty("PropName0", "PropValue0"));
-  EXPECT_EQ(tags[1], CustomProperty("PropName1", "PropValue1"));
-  EXPECT_EQ(tags[2], CustomProperty("PropName2", "PropValue2"));
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(3, properties.size());
+  EXPECT_EQ(properties[0], CustomProperty("PropName0", "PropValue0"));
+  EXPECT_EQ(properties[1], CustomProperty("PropName1", "PropValue1"));
+  EXPECT_EQ(properties[2], CustomProperty("PropName2", "PropValue2"));
 }
 
 const string wrongPropertyNodeXml = R"(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -64,10 +64,10 @@ const string wrongPropertyNodeXml = R"(<?xml version="1.0" encoding="UTF-8" stan
 </Properties>)";
 TEST(CustomProperties, GetProperties_WrongPropertyNode_NodeSkipped) {
   CustomPropertiesXml customProperties{ wrongPropertyNodeXml };
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(2, tags.size());
-  EXPECT_EQ(tags[0], CustomProperty("PropName0", "PropValue0"));
-  EXPECT_EQ(tags[1], CustomProperty("PropName2", "2"));
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(2, properties.size());
+  EXPECT_EQ(properties[0], CustomProperty("PropName0", "PropValue0"));
+  EXPECT_EQ(properties[1], CustomProperty("PropName2", "2"));
 }
 
 const string wrongPropertySubNodeXml = R"(<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -84,81 +84,81 @@ const string wrongPropertySubNodeXml = R"(<?xml version="1.0" encoding="UTF-8" s
 </Properties>)";
 TEST(CustomProperties, GetProperties_WrongPropertySubNode_NodeSkipped) {
   CustomPropertiesXml customProperties{ wrongPropertySubNodeXml };
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(2, tags.size());
-  EXPECT_EQ(tags[0], CustomProperty("PropName0", "PropValue0"));
-  EXPECT_EQ(tags[1], CustomProperty("PropName2", "2"));
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(2, properties.size());
+  EXPECT_EQ(properties[0], CustomProperty("PropName0", "PropValue0"));
+  EXPECT_EQ(properties[1], CustomProperty("PropName2", "2"));
 }
 
-TEST(CustomProperties, UpdateProperties_Delete_MultipleProperties_MultipleTags) {
+TEST(CustomProperties, UpdateProperties_DeleteMultipleProperties_OnlyUndeletedPropertiesLeft) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  vector<string> del{ "PropName0", "PropName2" };
-  vector<CustomProperty> vec;
-  customProperties.UpdateProperties(vec, del);
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(1, tags.size());
+  vector<string> keysToRemove{ "PropName0", "PropName2" };
+  vector<CustomProperty> propertiesToAdd;
+  customProperties.UpdateProperties(propertiesToAdd, keysToRemove);
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(1, properties.size());
+  EXPECT_EQ(properties[0], CustomProperty("PropName1", "PropValue1"));
 }
 
-TEST(CustomProperties, UpdateProperties_Delete_SamePropertyMultipleTimes_DoesntFail) {
+TEST(CustomProperties, UpdateProperties_DeleteSamePropertyMultipleTimes_DoesntFail) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  vector<string> del{ "PropName0", "PropName0" };
-  vector<CustomProperty> vec;
-  customProperties.UpdateProperties(vec, del);
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(2, tags.size());
+  vector<string> keysToRemove{ "PropName0", "PropName0" };
+  vector<CustomProperty> propertiesToAdd;
+  EXPECT_NO_THROW(customProperties.UpdateProperties(propertiesToAdd, keysToRemove));
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(2, properties.size());
 }
 
-TEST(CustomProperties, UpdateProperties_Delete_WrongProperty_DoesntFail) {
+TEST(CustomProperties, UpdateProperties_DeleteWrongProperty_DoesntFail) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  vector<string> del{ "PropNameX", "PropNameY" };
-  vector<CustomProperty> vec;
-  customProperties.UpdateProperties(vec, del);
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(3, tags.size());
+  vector<string> keysToRemove{ "PropNameX", "PropNameY" };
+  vector<CustomProperty> propertiesToAdd;
+  EXPECT_NO_THROW(customProperties.UpdateProperties(propertiesToAdd, keysToRemove));
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(3, properties.size());
 }
 
-TEST(CustomProperties, UpdateProperties_Add_MultipleProperties_MultipleTags) {
+TEST(CustomProperties, UpdateProperties_AddMultipleProperties_MultipleTagsAdded) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  vector<string> del;
-  vector<CustomProperty> vec { CustomProperty("PropName4", "PropValue4") , CustomProperty("PropName5", "PropValue5") };
-  customProperties.UpdateProperties(vec, del);
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(5, tags.size());
-  EXPECT_NE(tags.end(), std::find(tags.begin(), tags.end(), CustomProperty("PropName4", "PropValue4")));
-  EXPECT_NE(tags.end(), std::find(tags.begin(), tags.end(), CustomProperty("PropName5", "PropValue5")));
+  vector<string> keysToRemove;
+  vector<CustomProperty> propertiesToAdd { CustomProperty("PropName3", "PropValue3") , CustomProperty("PropName4", "PropValue4") };
+  customProperties.UpdateProperties(propertiesToAdd, keysToRemove);
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(5, properties.size());
+  EXPECT_EQ(properties[3], CustomProperty("PropName3", "PropValue3"));
+  EXPECT_EQ(properties[4], CustomProperty("PropName4", "PropValue4"));
 }
 
-TEST(CustomProperties, UpdateProperties_MultipleProperties_MultipleTags) {
+TEST(CustomProperties, UpdateProperties_MultipleAddDeleteProperties) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  vector<string> del{ "PropName0", "PropName2" };
-  vector<CustomProperty> vec{ CustomProperty("PropName4", "PropValue4") , CustomProperty("PropName5", "PropValue5") };
-  customProperties.UpdateProperties(vec, del);
-  auto tags = customProperties.GetProperties();
-  EXPECT_EQ(3, tags.size());
-  EXPECT_NE(tags.end(), std::find(tags.begin(), tags.end(), CustomProperty("PropName1", "PropValue1")));
-  EXPECT_NE(tags.end(), std::find(tags.begin(), tags.end(), CustomProperty("PropName4", "PropValue4")));
-  EXPECT_NE(tags.end(), std::find(tags.begin(), tags.end(), CustomProperty("PropName5", "PropValue5")));
+  vector<string> keysToRemove{ "PropName0", "PropName2" };
+  vector<CustomProperty> propertiesToAdd{ CustomProperty("PropName3", "PropValue3") , CustomProperty("PropName4", "PropValue4") };
+  customProperties.UpdateProperties(propertiesToAdd, keysToRemove);
+  auto properties = customProperties.GetProperties();
+  EXPECT_EQ(3, properties.size());
+  EXPECT_NE(properties.end(), std::find(properties.begin(), properties.end(), CustomProperty("PropName1", "PropValue1")));
+  EXPECT_NE(properties.end(), std::find(properties.begin(), properties.end(), CustomProperty("PropName3", "PropValue3")));
+  EXPECT_NE(properties.end(), std::find(properties.begin(), properties.end(), CustomProperty("PropName4", "PropValue4")));
 }
 
 const string expectdXml =
 R"(<?xml version="1.0" encoding="utf-8" standalone="yes"?>)"
 R"(<Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/custom-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes">)"
-R"(<property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="2" name="PropName1">)"
-R"(<vt:lpwstr>PropValue1</vt:lpwstr>)"
-R"(</property>)"
-R"(<property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="3" name="PropName4">)"
-R"(<vt:lpwstr>PropValue4</vt:lpwstr>)"
-R"(</property>)"
-R"(<property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="4" name="PropName5">)"
-R"(<vt:lpwstr>PropValue5</vt:lpwstr>)"
-R"(</property>)"
+  R"(<property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="2" name="PropName1">)"
+    R"(<vt:lpwstr>PropValue1</vt:lpwstr>)"
+  R"(</property>)"
+  R"(<property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="3" name="PropName3">)"
+    R"(<vt:lpwstr>PropValue3</vt:lpwstr>)"
+  R"(</property>)"
+  R"(<property fmtid="{D5CDD505-2E9C-101B-9397-08002B2CF9AE}" pid="4" name="PropName4">)"
+    R"(<vt:lpwstr>PropValue4</vt:lpwstr>)"
+  R"(</property>)"
 R"(</Properties>)";
-
 
 TEST(CustomProperties, UpdateProperties_Serialize_StringUpdated) {
   CustomPropertiesXml customProperties{ multiPropertyXml };
-  vector<string> del{ "PropName0", "PropName2" };
-  vector<CustomProperty> vec{ CustomProperty("PropName4", "PropValue4") , CustomProperty("PropName5", "PropValue5") };
-  customProperties.UpdateProperties(vec, del);
+  vector<string> keysToRemove{ "PropName0", "PropName2" };
+  vector<CustomProperty> propertiesToAdd{ CustomProperty("PropName3", "PropValue3") , CustomProperty("PropName4", "PropValue4") };
+  customProperties.UpdateProperties(propertiesToAdd, keysToRemove);
   EXPECT_STREQ(expectdXml.c_str(), customProperties.Serialize().c_str());
 }
