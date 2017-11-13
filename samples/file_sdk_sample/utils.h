99a826da04a729382eaee8759e14f127f1232312
MIDDLEEAST\aykeshet <aykeshet@microsoft.com>
2017-11-13 16:24:35 +0200

    Delete file_sample and changes in scripts

--- samples/file_sdk_sample/utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#include <string>
-
-bool icompare_pred(unsigned char a, unsigned char b);
-bool icompare(std::string const& a, std::string const& b);
-
-#endif // UTILS_H
